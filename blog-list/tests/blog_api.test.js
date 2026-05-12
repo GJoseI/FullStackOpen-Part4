1e6082dc157cec811e112c46a53e269b049d2c83
GJoseI <joseignaciogauna2@gmail.com>
2026-05-11 22:52:05 -0300

    4.10

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -28,6 +28,28 @@ test('id property is named correctly', async () => {
   assert(result)
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const title = blogsAtEnd.map(n => n.title)
+
+  assert(title.includes('Go To Statement Considered Harmful'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
