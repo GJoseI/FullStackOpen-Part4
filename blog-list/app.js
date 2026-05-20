0e2bb81bf8a9f750b6505b1a53f48b714708e2e8
GJoseI <joseignaciogauna2@gmail.com>
2026-05-20 20:13:35 -0300

    4.15

--- blog-list/app.js
+++ blog-list/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -22,6 +23,7 @@ app.use(express.json())
 app.use(middleware.morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
